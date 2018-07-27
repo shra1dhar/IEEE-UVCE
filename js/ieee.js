dbff78f64eb90439c475129d64d997cafdebccee
Shravan Dhar <shraonedhar@gmail.com>
2018-07-28 02:33:00 +0530

    major revamp | bootstrap removed | support for older browser included

--- js/ieee.js
+++ js/ieee.js
@@ -1,5 +1,63 @@
-$(document).ready(function(){
-      if (window.matchMedia("(max-width: 495px)").matches) {
-        $("body").removeClass("container").addClass("container-fluid");
-      }
+  // ************************************************************************
+  // Copy of this code in embeded inline in index.html to increase efficiency
+  // ************************************************************************
+
+$(".bars").hide();
+$(".upper-section").hide();
+$("#sig-box").hide();
+$("#kagada-impetus").hide();
+$("#awards").hide();
+$("#team").hide();
+$("#execom").hide();
+$("#repcom").hide();
+$("#foot").hide();
+
+$(document).ready( () => {
+  $(".svg-wrapper").click( () => {
+    $(".overlay").hide('slow');
+    $(".film").css("display", "block");
+    $(".film").addClass("light");
+    setTimeout( () => {
+      $(".bars").show();
+      $(".upper-section").show();
+      $("#sig-box").show();
+      $("#kagada-impetus").show();
+      $("#awards").show();
+      $("#team").show();
+      $("#execom").show();
+      $("#repcom").show();
+      $("#foot").show();
+      $(".film").hide();
+      new WOW().init();
+    }, 3000);
+  });
 });
+
+function membersAppear(){
+  $(".bars").hide();
+  $(".upper-section").hide();
+  $("#sig-box").hide();
+  $("#kagada-impetus").hide();
+  $("#awards").hide();
+  $("#team").hide();
+  $("#foot").hide();
+  $("#members").css("display", "block");
+  // setTimeout( () => {
+  //   document.body.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+  // }, 0);
+  $("#members").css("animation", "appear 1s ease-in 0s");
+}
+
+function membersHide() {
+  $("#members").css("animation", "reverseAppear 0.7s ease-in 0s");
+  $("#members").css("display", "none");
+  $(".bars").show();
+  $(".upper-section").show();
+  $("#sig-box").show();
+  $("#kagada-impetus").show();
+  $("#awards").show();
+  $("#team").show();
+  $("#foot").show();
+}
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Description
+This is the source code for the Official Website of IEEE UVCE.
